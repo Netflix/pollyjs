5c1de61523ca9308175e43b7a9e1f9cd85521ea3
offirgolan <offirgolan@gmail.com>
2018-06-04 10:16:00 -0700

    chore: Add @commitlint/config-lerna-scopes.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,10 @@
 /* eslint-env node */
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-lerna-scopes',
+    '@commitlint/config-conventional'
+  ],
   rules: {
     'subject-full-stop': [2, 'always', '.'],
     'subject-case': [2, 'always', ['sentence-case']]
