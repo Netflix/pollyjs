65e699644edfb087f1a38dbad8ac29613fdc6c50
offirgolan <offirgolan@gmail.com>
2018-06-08 11:24:18 -0700

    revert: "Update commitlint.config.js"
    This reverts commit 24a76bad1a18d71e79ed24eecda11448c4094e8f.

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,8 @@ module.exports = {
   extends: [
     '@commitlint/config-lerna-scopes',
     '@commitlint/config-conventional'
-  ]
+  ],
+  rules: {
+    'subject-case': [2, 'always', ['sentence-case']]
+  }
 };
