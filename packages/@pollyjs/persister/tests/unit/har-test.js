42751d8c6e6b563207ca751e84a5190b1698c04d
offirgolan <offirgolan@gmail.com>
2018-06-29 17:49:50 -0700

    test: Add addEntries and sortEntries test cases

--- packages/@pollyjs/persister/tests/unit/har-test.js
+++ packages/@pollyjs/persister/tests/unit/har-test.js
@@ -24,9 +24,79 @@ describe('Unit | HAR', function() {
     });
 
     it('should be valid', async function() {
-      expect(await validate.log(new Log({
-        creator: { name: 'Polly.JS', version: '0.0.0' }
-      }))).to.be.true;
+      expect(
+        await validate.log(
+          new Log({
+            creator: { name: 'Polly.JS', version: '0.0.0' }
+          })
+        )
+      ).to.be.true;
+    });
+
+    it('addEntries: Entries should be unique & sorted', async function() {
+      const now = new Date().getTime();
+      const log = new Log({
+        entries: [
+          {
+            _id: 'abc',
+            _order: 0,
+            startedDateTime: new Date(now),
+            _new: false
+          },
+          {
+            _id: 'def',
+            _order: 0,
+            startedDateTime: new Date(now + 10),
+            _new: false
+          }
+        ]
+      });
+
+      log.addEntries([
+        {
+          _id: 'abc',
+          _order: 0,
+          startedDateTime: new Date(now + 100),
+          _new: true
+        },
+        {
+          _id: 'abc',
+          _order: 1,
+          startedDateTime: new Date(now + 105),
+          _new: true
+        },
+        { _id: 'ghi', _order: 0, startedDateTime: new Date(now), _new: true }
+      ]);
+
+      expect(
+        log.entries.map(({ _id, _order, _new }) => ({ _id, _order, _new }))
+      ).to.include.deep.ordered.members([
+        { _id: 'ghi', _order: 0, _new: true },
+        { _id: 'def', _order: 0, _new: false },
+        { _id: 'abc', _order: 0, _new: true },
+        { _id: 'abc', _order: 1, _new: true }
+      ]);
+    });
+
+    it('sortEntries: sorts by startedDateTime', async function() {
+      const now = new Date().getTime();
+      const log = new Log({
+        entries: [
+          { _id: 'd', startedDateTime: new Date(now + 15) },
+          { _id: 'b', startedDateTime: new Date(now + 5) },
+          { _id: 'a', startedDateTime: new Date(now + 0) },
+          { _id: 'c', startedDateTime: new Date(now + 10) }
+        ]
+      });
+
+      log.sortEntries();
+
+      expect(log.entries.map(e => e._id)).to.have.deep.ordered.members([
+        'a',
+        'b',
+        'c',
+        'd'
+      ]);
     });
   });
 });
