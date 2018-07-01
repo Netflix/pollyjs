ebe08259b0b437e8cebe79f19c8051884e65856c
offirgolan <offirgolan@gmail.com>
2018-07-01 00:47:03 -0700

    docs: Cleanup and add docs

--- packages/@pollyjs/core/src/-private/event-emitter.js
+++ packages/@pollyjs/core/src/-private/event-emitter.js
@@ -9,14 +9,12 @@ function assertEventName(eventName, eventNames) {
     typeof eventName === 'string'
   );
 
-  if (eventNames.size > 0) {
-    assert(
-      `Invalid event name provided: "${eventName}". Possible events: ${[
-        ...eventNames
-      ].join(', ')}.`,
-      eventNames.has(eventName)
-    );
-  }
+  assert(
+    `Invalid event name provided: "${eventName}". Possible events: ${[
+      ...eventNames
+    ].join(', ')}.`,
+    eventNames.has(eventName)
+  );
 }
 
 function assertListener(listener) {
@@ -27,15 +25,38 @@ function assertListener(listener) {
 }
 
 export default class EventEmitter {
+  /**
+   * @constructor
+   * @param {Object} options
+   * @param {String[]} options.eventNames - Supported events
+   */
   constructor({ eventNames = [] }) {
+    assert(
+      'An array of supported events must be provided via the `eventNames` option.',
+      eventNames && eventNames.length > 0
+    );
+
     this[EVENTS] = new Map();
     this[EVENT_NAMES] = new Set(eventNames);
   }
 
+  /**
+   * Returns an array listing the supported events.
+   *
+   * @returns {String[]}
+   */
   eventNames() {
     return [...this[EVENT_NAMES]];
   }
 
+  /**
+   * Adds the `listener` function to the end of the listeners array for t
+   * he event named `eventName`
+   *
+   * @param {String} eventName - The name of the event.
+   * @param {Function} listener - The callback function
+   * @returns {EventEmitter}
+   */
   on(eventName, listener) {
     assertEventName(eventName, this[EVENT_NAMES]);
     assertListener(listener);
@@ -46,15 +67,20 @@ export default class EventEmitter {
       events.set(eventName, new Set());
     }
 
-    if (!this[EVENT_NAMES].has(eventName)) {
-      this[EVENT_NAMES].add(eventName);
-    }
-
     events.get(eventName).add(listener);
 
     return this;
   }
 
+  /**
+   * Adds a one-time `listener` function for the event named `eventName`.
+   * The next time `eventName` is triggered, this listener is removed and
+   * then invoked.
+   *
+   * @param {String} eventName - The name of the event.
+   * @param {Function} listener - The callback function
+   * @returns {EventEmitter}
+   */
   once(eventName, listener) {
     assertListener(listener);
 
@@ -66,6 +92,15 @@ export default class EventEmitter {
     return this;
   }
 
+  /**
+   * Removes the specified `listener` from the listener array for
+   * the event named `eventName`. If `listener` is not provided then it removes
+   * all listeners, or those of the specified `eventName`.
+   *
+   * @param {String} eventName - The name of the event.
+   * @param {Function} [listener] - The callback function
+   * @returns {EventEmitter}
+   */
   off(eventName, listener) {
     assertEventName(eventName, this[EVENT_NAMES]);
 
@@ -82,25 +117,72 @@ export default class EventEmitter {
     return this;
   }
 
+  /**
+   * Returns a copy of the array of listeners for the event named `eventName`.
+   *
+   * @param {String} eventName - The name of the event.
+   * @returns {Function[]}
+   */
   listeners(eventName) {
     return this.hasListeners(eventName) ? [...this[EVENTS].get(eventName)] : [];
   }
 
+  /**
+   * Returns `true` if there are any listeners for the event named `eventName`
+   * or `false` otherwise.
+   *
+   * @param {String} eventName - The name of the event.
+   * @returns {Boolean}
+   */
   hasListeners(eventName) {
     const events = this[EVENTS];
 
     return events.has(eventName) && events.get(eventName).size > 0;
   }
 
+  /**
+   * Asynchronously calls each of the `listeners` registered for the event named
+   * `eventName`, in the order they were registered, passing the supplied
+   * arguments to each.
+   *
+   * Returns a promise that will resolve to `true` if the event had listeners,
+   * `false` otherwise.
+   *
+   * @param {String} eventName - The name of the event.
+   * @returns {Promise}
+   */
   async emit(eventName, ...args) {
-    for (const listener of this.listeners(eventName)) {
-      await listener(...args);
+    if (this.hasListeners(eventName)) {
+      for (const listener of this.listeners(eventName)) {
+        await listener(...args);
+      }
+
+      return true;
     }
+
+    return false;
   }
 
+  /**
+   * Asynchronously and concurrently calls each of the `listeners` registered
+   * for the event named `eventName`, in the order they were registered,
+   * passing the supplied arguments to each.
+   *
+   * Returns a promise that will resolve to `true` if the event had listeners,
+   * `false` otherwise.
+   *
+   * @param {String} eventName - The name of the event.
+   * @returns {Promise}
+   */
   async emitParallel(eventName, ...args) {
-    await Promise.all(
-      this.listeners(eventName).map(listener => listener(...args))
-    );
+    if (this.hasListeners(eventName)) {
+      await Promise.all(
+        this.listeners(eventName).map(listener => listener(...args))
+      );
+
+      return true;
+    }
+
+    return false;
   }
 }
