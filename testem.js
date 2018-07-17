9f3937d20e301d3188e2ef60db805c343baedbe2
offirgolan <offirgolan@gmail.com>
2018-07-17 14:14:58 -0700

    chore: Build it all

--- testem.js
+++ testem.js
@@ -4,7 +4,7 @@ module.exports = {
   fail_on_zero_tests: true,
   test_page: 'tests/index.mustache',
   before_tests: 'npm-run-all --parallel build test:build',
-  launch_in_ci: ['Node', 'Ember', 'ESLint'],
+  launch_in_ci: ['Chrome', 'Node', 'Ember', 'ESLint'],
   launch_in_dev: ['Chrome', 'Node', 'Ember', 'ESLint'],
   watch_files: [
     './build-scripts/**/*',
