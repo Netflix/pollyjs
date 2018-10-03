0a99a0bd5efcab7a283302aed7fd242967fcb882
Sergey Petushkov <petushkov.sergey@gmail.com>
2018-10-03 11:14:42 +0200

    docs: Separate recordings for each test, change suite name

--- examples/jest-node-fetch/__tests__/index.test.js
+++ examples/jest-node-fetch/__tests__/index.test.js
@@ -9,8 +9,8 @@ Polly.register(FSPersister);
 
 const { posts, users } = require('../src');
 
-describe('src', () => {
-  const polly = new Polly('jsonplaceholder', {
+const setupPolly = name => {
+  return new Polly(name, {
     adapters: ['fetch'],
     persister: 'fs',
     persisterOptions: {
@@ -19,27 +19,35 @@ describe('src', () => {
       }
     }
   });
+};
 
-  afterAll(async () => {
-    await polly.stop();
-  });
-
+describe('jest-node-fetch', () => {
   describe('posts', () => {
     it('should return post', async () => {
+      const polly = setupPolly('jest-node-fetch/posts/should return post');
+
       const post = await posts('1');
 
       expect(post.id).toBe(1);
-      expect(post.title).toBe('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+      expect(post.title).toBe(
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+      );
+
+      await polly.stop();
     });
   });
 
   describe('users', () => {
     it('should return user', async () => {
+      const polly = setupPolly('jest-node-fetch/users/should return user');
+
       const user = await users('1');
 
       expect(user.id).toBe(1);
       expect(user.name).toBe('Leanne Graham');
       expect(user.email).toBe('Sincere@april.biz');
+
+      await polly.stop();
     });
   });
 });
