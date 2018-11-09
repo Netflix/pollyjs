6c434c12d43ca93075263e2603fd5b9687a80044
offirgolan <offirgolan@gmail.com>
2018-11-09 10:05:22 -0800

    test: Fix hanging jest test case

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@
 module.exports = {
   testURL: 'http://localhost:4000/api',
   testMatch: ['**/@pollyjs/*/build/jest/*.js'],
+  roots: ['<rootDir>/packages/@pollyjs'],
   reporters: ['jest-tap-reporter']
 };
