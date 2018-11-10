de64529f5c8db374263644529cdc1e369b53ade4
offirgolan <offirgolan@gmail.com>
2018-11-09 19:35:22 -0800

    chore(ember): Bump ember version to 3.5

--- testem.js
+++ testem.js
@@ -22,9 +22,12 @@ module.exports = {
     Chrome: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.TRAVIS ? '--no-sandbox' : null,
-        '--disable-gpu',
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
       ].filter(Boolean)
