3572f9c49e281d1c4e9b889f306ca6efebfb6ca8
Yasin Uslu <nepjua@gmail.com>
2018-11-12 23:21:59 +0300

    test(adapter-node-http): Add test case

--- packages/@pollyjs/adapter-http/tests/node/integration/adapter-test.js
+++ packages/@pollyjs/adapter-http/tests/node/integration/adapter-test.js
@@ -1,3 +1,24 @@
+import '@pollyjs-tests/helpers/global-fetch';
+
+import setupFetchRecord from '@pollyjs-tests/helpers/setup-fetch-record';
+import adapterTests from '@pollyjs-tests/integration/adapter-tests';
+import FSPersister from '@pollyjs/persister-fs';
+import { setupMocha as setupPolly } from '@pollyjs/core';
+
+import HttpAdapter from '../../../src';
+
 describe('Integration | Http Adapter | Node', function() {
-  // TODO : Write tests for adapter-http
+  setupPolly.beforeEach({
+    recordFailedRequests: true,
+    adapters: [HttpAdapter],
+    persister: FSPersister,
+    persisterOptions: {
+      fs: { recordingsDir: 'tests/recordings' }
+    }
+  });
+
+  setupFetchRecord({ host: 'http://localhost:4000' });
+  setupPolly.afterEach();
+
+  adapterTests();
 });
