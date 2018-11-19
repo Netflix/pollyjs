40d62b96b4f51c6b1c38b5d32319e84b9b454ad7
offirgolan <offirgolan@gmail.com>
2018-11-18 22:24:10 -0800

    feat(core): Support custom functions in matchRequestsBy config options

--- packages/@pollyjs/core/src/utils/normalize-request.js
+++ packages/@pollyjs/core/src/utils/normalize-request.js
@@ -1,4 +1,5 @@
 import isObjectLike from 'lodash-es/isObjectLike';
+import isFunction from 'lodash-es/isFunction';
 import stringify from 'fast-json-stable-stringify';
 
 import parseUrl from './parse-url';
@@ -8,15 +9,23 @@ const { keys } = Object;
 const { isArray } = Array;
 const { parse } = JSON;
 
-export function method(method) {
-  return (method || 'GET').toUpperCase();
+export function method(method, config) {
+  return isFunction(config)
+    ? config(method || 'GET')
+    : (method || 'GET').toUpperCase();
 }
 
 export function url(url, config = {}) {
   const parsedUrl = parseUrl(url, true);
 
   // Remove any url properties that have been disabled via the config
-  keys(config).forEach(key => !config[key] && parsedUrl.set(key, ''));
+  keys(config).forEach(key => {
+    if (isFunction(config[key])) {
+      parsedUrl.set(key, config[key](parsedUrl[key]));
+    } else if (!config[key]) {
+      parsedUrl.set(key, '');
+    }
+  });
 
   // Sort Query Params
   if (isObjectLike(parsedUrl.query)) {
@@ -31,18 +40,23 @@ export function headers(headers, config) {
 
   if (isObjectLike(normalizedHeaders)) {
     normalizedHeaders = new HTTPHeaders(normalizedHeaders);
+  }
 
-    // Filter out excluded headers
-    if (isObjectLike(config) && isArray(config.exclude)) {
-      config.exclude.forEach(header => (normalizedHeaders[header] = null));
-    }
+  if (isFunction(config)) {
+    normalizedHeaders = config(normalizedHeaders);
+  } else if (
+    isObjectLike(normalizedHeaders) &&
+    isObjectLike(config) &&
+    isArray(config.exclude)
+  ) {
+    config.exclude.forEach(header => delete normalizedHeaders[header]);
   }
 
   return normalizedHeaders;
 }
 
-export function body(body) {
-  return body;
+export function body(body, config) {
+  return isFunction(config) ? config(body) : body;
 }
 
 export default {
--- packages/@pollyjs/core/tests/unit/utils/normalize-request-test.js
+++ packages/@pollyjs/core/tests/unit/utils/normalize-request-test.js
@@ -26,6 +26,10 @@ describe('Unit | Utils | Normalize Request', function() {
       expect(method('delete')).to.equal('DELETE');
       expect(method('option')).to.equal('OPTION');
     });
+
+    it('should support a custom fn', function() {
+      expect(method('GET', m => m.toLowerCase())).to.equal('get');
+    });
   });
 
   describe('headers', function() {
@@ -53,6 +57,23 @@ describe('Unit | Utils | Normalize Request', function() {
         )
       ).to.deep.equal({ accept: 'foo' });
     });
+
+    it('should support a custom fn', function() {
+      expect(
+        headers(
+          {
+            Accept: 'foo',
+            test: 'test',
+            'Content-Type': 'Bar'
+          },
+          headers => {
+            delete headers.test;
+
+            return headers;
+          }
+        )
+      ).to.deep.equal({ accept: 'foo', 'content-type': 'Bar' });
+    });
   });
 
   describe('url', function() {
@@ -62,7 +83,7 @@ describe('Unit | Utils | Normalize Request', function() {
       );
     });
 
-    it('should respect `hash` config', function() {
+    it('should respect `matchRequestsBy.url` config', function() {
       [
         [
           /* config options */
@@ -72,43 +93,54 @@ describe('Unit | Utils | Normalize Request', function() {
           /* expected when true */
           [true, 'http://hash-test.com?a=1&b=1&c=1#hello=world'],
           /* expected when false */
-          [false, 'http://hash-test.com?a=1&b=1&c=1']
+          [false, 'http://hash-test.com?a=1&b=1&c=1'],
+          /* expected when custom fn */
+          [
+            h => h.replace('=', '!='),
+            'http://hash-test.com?a=1&b=1&c=1#hello!=world'
+          ]
         ],
         [
           'protocol',
           'http://protocol-test.com',
           [true, 'http://protocol-test.com'],
-          [false, '//protocol-test.com']
+          [false, '//protocol-test.com'],
+          [p => p.replace('http', 'https'), 'https://protocol-test.com']
         ],
         [
           'query',
           'http://query-test.com?b=1&c=1&a=1',
           [true, 'http://query-test.com?a=1&b=1&c=1'],
-          [false, 'http://query-test.com']
+          [false, 'http://query-test.com'],
+          [q => ({ ...q, c: 2 }), 'http://query-test.com?a=1&b=1&c=2']
         ],
         [
           'username',
           'https://username:password@username-test.com',
           [true, 'https://username:password@username-test.com'],
-          [false, 'https://username-test.com']
+          [false, 'https://username-test.com'],
+          [u => `${u}123`, 'https://username123:password@username-test.com']
         ],
         [
           'password',
           'https://username:password@password-test.com',
           [true, 'https://username:password@password-test.com'],
-          [false, 'https://username@password-test.com']
+          [false, 'https://username@password-test.com'],
+          [p => `${p}123`, 'https://username:password123@password-test.com']
         ],
         [
           'port',
           'https://port-test.com:8000',
           [true, 'https://port-test.com:8000'],
-          [false, 'https://port-test.com']
+          [false, 'https://port-test.com'],
+          [p => Number(p) + 1, 'https://port-test.com:8001']
         ],
         [
           'pathname',
           'https://pathname-test.com/bar/baz',
           [true, 'https://pathname-test.com/bar/baz'],
-          [false, 'https://pathname-test.com']
+          [false, 'https://pathname-test.com'],
+          [p => p.replace('bar', 'foo'), 'https://pathname-test.com/foo/baz']
         ]
       ].forEach(([rule, input, ...options]) => {
         options.forEach(([optionValue, expectedOutput]) => {
@@ -119,6 +151,12 @@ describe('Unit | Utils | Normalize Request', function() {
 
     it('should respect relative urls', function() {
       expect(url('/some/path')).to.equal('/some/path');
+    });
+  });
+
+  describe('body', function() {
+    it('should support a custom fn', function() {
+      expect(body('foo', b => b.toUpperCase())).to.equal('FOO');
     });
   });
 });
