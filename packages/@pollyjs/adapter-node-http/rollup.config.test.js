2136df4c81a9b470fd2bc5a8b680ceea0ba0b31e
Yasin Uslu <nepjua@gmail.com>
2018-11-25 19:42:03 +0300

    test: Add binary download tests

--- packages/@pollyjs/adapter-node-http/rollup.config.test.js
+++ packages/@pollyjs/adapter-node-http/rollup.config.test.js
@@ -1,5 +1,5 @@
 import createNodeTestConfig from '../../../build-scripts/rollup.node.test.config';
 
 export default createNodeTestConfig({
-  external: ['http', 'https', 'url']
+  external: ['http', 'https', 'url', 'crypto']
 });
--- packages/@pollyjs/adapter-node-http/tests/node/integration/adapter-test.js
+++ packages/@pollyjs/adapter-node-http/tests/node/integration/adapter-test.js
@@ -1,11 +1,72 @@
 import '@pollyjs-tests/helpers/global-fetch';
 
+import http from 'http';
+import https from 'https';
+import crypto from 'crypto';
+
 import setupFetchRecord from '@pollyjs-tests/helpers/setup-fetch-record';
 import adapterTests from '@pollyjs-tests/integration/adapter-tests';
 import FSPersister from '@pollyjs/persister-fs';
 import { setupMocha as setupPolly } from '@pollyjs/core';
 
 import NodeHttpAdapter from '../../../src';
+
+const collectBuffer = stream => {
+  return new Promise(resolve => {
+    const chunks = [];
+
+    stream.on('data', chunk => {
+      chunks.push(chunk);
+    });
+
+    stream.on('end', () => {
+      resolve(Buffer.concat(chunks));
+    });
+  });
+};
+
+const calculateHashFromStream = async stream => {
+  const hmac = crypto.createHmac('sha256', 'a secret');
+  const hashStream = stream.pipe(hmac);
+
+  const hashBuffer = await collectBuffer(hashStream);
+
+  return hashBuffer.toString('hex');
+};
+
+const createHttpResponseStream = req =>
+  new Promise(resolve => {
+    req.on('response', res => {
+      resolve(res);
+    });
+
+    req.end();
+  });
+
+const binaryDownloadTest = (testTitle, transport, url) => {
+  it(testTitle, async function() {
+    const { server } = this.polly;
+
+    server.get(url).passthrough(true);
+
+    const nativeResponseStream = await createHttpResponseStream(
+      transport.request(url)
+    );
+
+    server.get(url).passthrough(false);
+
+    const recordedResponseStream = await createHttpResponseStream(
+      transport.request(url)
+    );
+
+    const [nativeHash, recordedHash] = await Promise.all([
+      calculateHashFromStream(nativeResponseStream),
+      calculateHashFromStream(recordedResponseStream)
+    ]);
+
+    expect(nativeHash).to.equal(recordedHash);
+  });
+};
 
 describe('Integration | Node Http Adapter | Node', function() {
   setupPolly.beforeEach({
@@ -21,4 +82,16 @@ describe('Integration | Node Http Adapter | Node', function() {
   setupPolly.afterEach();
 
   adapterTests();
+
+  binaryDownloadTest(
+    'should be able to fetch binary content with http',
+    http,
+    'http://via.placeholder.com/150/92c952'
+  );
+
+  binaryDownloadTest(
+    'should be able to fetch binary content with https',
+    https,
+    'https://via.placeholder.com/150/92c952'
+  );
 });
