df81f5ef39b59ab3f9493a0883ad6fa4e647fe2b
Brandon Chinn <brandon@leapyear.io>
2020-01-29 14:32:07 -0800

    fix(adapter-node-http): Fix JSON values that look like hexadecimal

--- packages/@pollyjs/adapter-node-http/src/utils/request-body.js
+++ packages/@pollyjs/adapter-node-http/src/utils/request-body.js
@@ -2,17 +2,17 @@ import { isUtf8Representable, isJSONContent } from 'nock/lib/common';
 
 export function parseBody(body, headers) {
   if (body) {
-    if (
+    if (isJSONContent(headers)) {
+      // Nock will parse json content into an object. We have our own way
+      // of dealing with json content so convert it back to a string.
+      return JSON.stringify(body);
+    } else if (
       typeof body === 'string' &&
       !isUtf8Representable(Buffer.from(body, 'hex'))
     ) {
       // Nock internally converts a binary buffer into its hexadecimal
       // representation so convert it back to a buffer.
       return Buffer.from(body, 'hex');
-    } else if (isJSONContent(headers)) {
-      // Nock will parse json content into an object. We have our own way
-      // of dealing with json content so convert it back to a string.
-      return JSON.stringify(body);
     }
   }
   return body;
