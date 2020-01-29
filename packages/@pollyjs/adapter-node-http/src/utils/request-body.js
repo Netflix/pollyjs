12d6a5df9ea621528cd292193d3d4a32c04a570e
Brandon Chinn <brandon@leapyear.io>
2020-01-29 15:32:01 -0800

    fix(adapter-node-http): Explicitly check nullish values

--- packages/@pollyjs/adapter-node-http/src/utils/request-body.js
+++ packages/@pollyjs/adapter-node-http/src/utils/request-body.js
@@ -1,19 +1,22 @@
 import { isUtf8Representable, isJSONContent } from 'nock/lib/common';
 
 export function parseBody(body, headers) {
-  if (body) {
-    if (isJSONContent(headers)) {
-      // Nock automatically parses json content, but we have our own way
-      // of dealing with json content, so convert it back to a string.
-      return JSON.stringify(body);
-    } else if (
-      typeof body === 'string' &&
-      !isUtf8Representable(Buffer.from(body, 'hex'))
-    ) {
-      // Nock automatically converts a binary buffer into its hexadecimal
-      // representation so convert it back to a buffer.
-      return Buffer.from(body, 'hex');
-    }
+  if (body === null || body === undefined) {
+    return body;
   }
+
+  if (isJSONContent(headers)) {
+    // Nock automatically parses json content, but we have our own way
+    // of dealing with json content, so convert it back to a string.
+    return JSON.stringify(body);
+  } else if (
+    typeof body === 'string' &&
+    !isUtf8Representable(Buffer.from(body, 'hex'))
+  ) {
+    // Nock automatically converts a binary buffer into its hexadecimal
+    // representation so convert it back to a buffer.
+    return Buffer.from(body, 'hex');
+  }
+
   return body;
 }
