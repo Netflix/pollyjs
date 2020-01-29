168b948ceffe029cc11ddf71d4dd08d03ee921dc
Brandon Chinn <brandon@leapyear.io>
2020-01-29 14:32:47 -0800

    fix(adapter-node-http): Update comments

--- packages/@pollyjs/adapter-node-http/src/utils/request-body.js
+++ packages/@pollyjs/adapter-node-http/src/utils/request-body.js
@@ -3,14 +3,14 @@ import { isUtf8Representable, isJSONContent } from 'nock/lib/common';
 export function parseBody(body, headers) {
   if (body) {
     if (isJSONContent(headers)) {
-      // Nock will parse json content into an object. We have our own way
-      // of dealing with json content so convert it back to a string.
+      // Nock automatically parses json content, but we have our own way
+      // of dealing with json content, so convert it back to a string.
       return JSON.stringify(body);
     } else if (
       typeof body === 'string' &&
       !isUtf8Representable(Buffer.from(body, 'hex'))
     ) {
-      // Nock internally converts a binary buffer into its hexadecimal
+      // Nock automatically converts a binary buffer into its hexadecimal
       // representation so convert it back to a buffer.
       return Buffer.from(body, 'hex');
     }
