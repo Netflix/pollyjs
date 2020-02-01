0bb2f5a93f44c0291a4ec14ade198e5b0abcdf56
Jason Mitchell <jason.mitchell.w@gmail.com>
2020-02-01 15:35:51 -0800

    ci: Adds back orphaned node adapter specifics test (#306)
    * ci: Adds back orphaned node adapter specifics test
    
    * chore: Rename global-fetch to global-node-fetch

--- tests/integration/adapter-node-tests.js
+++ tests/integration/adapter-node-tests.js
@@ -4,17 +4,18 @@ export default function adapterNodeTests() {
   it('should handle recording requests posting a Buffer', async function() {
     const { server, recordingName } = this.polly;
     const buffer = Buffer.from(recordingName);
+    const url = `http://example.com/upload`;
 
-    server.post('/submit').intercept((req, res) => {
+    server.post(url).intercept((req, res) => {
       const body = req.identifiers.body;
 
       // Make sure the buffer exists in the identifiers
-      expect(body).to.include(buffer.toString('hex'));
+      expect(body).to.include(buffer.toString());
 
       res.sendStatus(200);
     });
 
-    const res = await this.fetch('/submit', { method: 'POST', body: buffer });
+    const res = await this.fetch(url, { method: 'POST', body: buffer });
 
     expect(res.status).to.equal(200);
   });
@@ -22,17 +23,18 @@ export default function adapterNodeTests() {
   it('should handle recording requests posting an ArrayBuffer', async function() {
     const { server } = this.polly;
     const buffer = new ArrayBuffer(8);
+    const url = `http://example.com/upload`;
 
-    server.post('/submit').intercept((req, res) => {
+    server.post(url).intercept((req, res) => {
       const body = req.identifiers.body;
 
       // Make sure the buffer exists in the identifiers
-      expect(body).to.include(Buffer.from(buffer).toString('hex'));
+      expect(body).to.include(Buffer.from(buffer).toString());
 
       res.sendStatus(200);
     });
 
-    const res = await this.fetch('/submit', { method: 'POST', body: buffer });
+    const res = await this.fetch(url, { method: 'POST', body: buffer });
 
     expect(res.status).to.equal(200);
   });
