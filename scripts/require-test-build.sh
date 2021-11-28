d7b550aca375b2cc961b4cba3766c8b6635ea0b8
Offir Golan <offirgolan@gmail.com>
2021-11-28 11:34:12 -0700

    chore: Cleanup npm scripts

--- scripts/require-test-build.sh
+++ scripts/require-test-build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ ! -f "./packages/@pollyjs/node-server/dist/cjs/pollyjs-node-server.js" ]; then
-  echo "pollyjs: test server build not found; run either '$ yarn watch' or '$ yarn server:build'"
+  echo "Test server build not found. Run either '$ yarn watch' or '$ yarn build:server'"
   exit 1
 fi
 
 if [ ! -f "./packages/@pollyjs/core/dist/cjs/pollyjs-core.js" ]; then
-  echo "pollyjs: build not found; run either '$ yarn watch' or '$ yarn build'"
+  echo "Build not found. Run either '$ yarn watch' or '$ yarn build'"
   exit 1
 fi
 
 if [ ! -f "./packages/@pollyjs/core/build/node/test-bundle.cjs.js" ]; then
-  echo "pollyjs: test build not found; run either '$ yarn watch' or '$ yarn test:build'"
+  echo "Test build not found. Run either '$ yarn watch' or '$ yarn test:build'"
   exit 1
 fi
