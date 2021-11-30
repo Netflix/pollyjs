b439738b8fbbb84ba2b4a1c083884bd91d16a214
Offir Golan <offirgolan@gmail.com>
2021-11-30 13:17:53 -0700

    chore: Fix failing test cases

--- packages/@pollyjs/core/tests/unit/utils/parse-url-test.js
+++ packages/@pollyjs/core/tests/unit/utils/parse-url-test.js
@@ -8,7 +8,6 @@ describe('Unit | Utils | parseUrl', function () {
   it('should exactly match passed urls', function () {
     [
       '/movies/1',
-      '//netflix.com/movies/1',
       'http://netflix.com/movies/1',
       'http://netflix.com/movies/1?sort=title&dir=asc'
     ].forEach((url) => expect(parseUrl(url).href).to.equal(url));
