e5b2f6209ca4c3996730d76ed4a9adcdfb2f027c
Dan Levy <dan.levy@Hotelengine.com>
2022-02-18 23:07:30 -0700

    refactor: Cleaning up POLLY_MODE logic

--- examples/typescript-jest-node-fetch/src/github-api.test.ts
+++ examples/typescript-jest-node-fetch/src/github-api.test.ts
@@ -1,32 +1,31 @@
 /** @jest-environment setup-polly-jest/jest-environment-node */
-import autoSetupPolly from "./utils/auto-setup-polly";
-import { getUser } from "./github-api";
+import autoSetupPolly from './utils/auto-setup-polly';
+import { getUser } from './github-api';
 
-// 🟡 Important: Run `autoSetupPolly()`/`setupPolly()` EARLY!
-let pollyContext = autoSetupPolly();
-/* Polly/Nock MUST patch HTTP modules before your code `import`'s or `require`'s those modules.
-✅ Place autoSetupPolly() after `import`'s, OR at the top of first `describe()`.
-❌ Do NOT autoSetupPolly() in `beforeAll`/`beforeEach`!
-*/
+describe('github-api client', () => {
+  let pollyContext = autoSetupPolly();
 
-// beforeEach(() => {
-//   // Common or shared interceptors go here, or in tests. See example below.
-//   pollyContext.polly.server
-//       .get("/ping")
-//       .intercept((req, res) => void res.sendStatus(200));
-// });
+  beforeEach(() => {
+    // Intercept /ping healthcheck requests (example)
+    pollyContext.polly.server
+      .any("/ping")
+      .intercept((req, res) => void res.sendStatus(200));
+  });
 
-test("getUser", async () => {
-  const user: any = await getUser("netflix");
-  expect(typeof user).toBe("object");
-  expect(user?.login).toBe("Netflix");
-});
+  it('getUser', async () => {
+    const user: any = await getUser('netflix');
+    expect(typeof user).toBe('object');
+    expect(user?.login).toBe('Netflix');
+  });
 
-test("getUser: custom interceptor", async () => {
-  expect.assertions(1);
-  pollyContext.polly.server
-    .get("https://api.github.com/users/failing_request_trigger")
-    .intercept((req, res) => void res.sendStatus(500));
+  it('getUser: custom interceptor', async () => {
+    expect.assertions(1);
+    pollyContext.polly.server
+      .get('https://api.github.com/users/failing_request_trigger')
+      .intercept((req, res) => void res.sendStatus(500));
 
-  await expect(getUser("failing_request_trigger")).rejects.toThrow('Http Error: 500');
+    await expect(getUser('failing_request_trigger')).rejects.toThrow(
+      'Http Error: 500'
+    );
+  });
 });
--- examples/typescript-jest-node-fetch/src/utils/auto-setup-polly.ts
+++ examples/typescript-jest-node-fetch/src/utils/auto-setup-polly.ts
@@ -1,37 +1,43 @@
 import path from "path";
 import { setupPolly } from "setup-polly-jest";
-import { Polly } from "@pollyjs/core";
+import { Polly, PollyConfig } from "@pollyjs/core";
 import NodeHttpAdapter from "@pollyjs/adapter-node-http";
 import FSPersister from "@pollyjs/persister-fs";
-// import FetchAdapter from "@pollyjs/adapter-fetch";
-// Polly.register(FetchAdapter);
+
 Polly.register(NodeHttpAdapter);
 Polly.register(FSPersister);
 
-const {POLLY_MODE} = process.env;
-
 let recordIfMissing = true;
-if (POLLY_MODE === "offline") recordIfMissing = false;
+let mode: PollyConfig['mode'] = 'replay';
 
-const pollyMode = POLLY_MODE === "record" ? "record"
-                : POLLY_MODE === "offline" ? "replay"
-                : "replay"; // Default is "replay"
+switch (process.env.POLLY_MODE) {
+  case 'record':
+    mode = 'record';
+    break;
+  case 'replay':
+    mode = 'replay';
+    break;
+  case 'offline':
+    mode = 'replay';
+    recordIfMissing = false;
+    break;
+}
 
 export default function autoSetupPolly() {
   /**
-   * NOTE: Customize your config here, usually differs per project.
-   * 
    * This persister can be adapted for both Node.js and Browser environments.
+   * 
+   * TODO: Customize your config.
    */
   return setupPolly({
-    // 🟡 Note: In node, all `fetch` like libraries use the http/https modules under the hood.
-    // `node-fetch` is handled by the `NodeHttpAdapter`, NOT the `FetchAdapter`.
+    // 🟡 Note: In node, most `fetch` like libraries use the http/https modules.
+    // `node-fetch` is handled by `NodeHttpAdapter`, NOT the `FetchAdapter`.
     adapters: ["node-http"],
+    mode,
+    recordIfMissing,
     flushRequestsOnStop: true,
     logging: false,
-    mode: pollyMode,
     recordFailedRequests: true,
-    recordIfMissing,
     persister: "fs",
     persisterOptions: {
       fs: {
@@ -40,12 +46,3 @@ export default function autoSetupPolly() {
     },
   });
 }
-
-/*
-If you have trouble initializing/loading the interceptors early enough, try a singleton pattern for this module:
-
-1. Remove the `export default` here: `export default function autoSetupPolly() {:`
-2. Add a new line: `export default autoSetupPolly();`
-
-Then when you `import` this module it will execute roughly immediately, and the default export will be the Context.
-*/
